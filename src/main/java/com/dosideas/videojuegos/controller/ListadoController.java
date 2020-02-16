db9ad3b794e2a5e569c011c1351344c2c5ca2bdf
NicoGullino <43994767+NicoGullino@users.noreply.github.com>
2020-02-16 13:32:21 -0300

    Add files via upload

---
+++ src/main/java/com/dosideas/videojuegos/controller/ListadoController.java
@@ -0,0 +1,24 @@
+package com.dosideas.videojuegos.controller;
+
+import com.dosideas.videojuegos.domain.Videojuego;
+import com.dosideas.videojuegos.service.VideojuegoService;
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller 
+public class ListadoController {
+    
+    private final VideojuegoService videojuegoService;
+
+    public ListadoController(VideojuegoService videojuegoService) {
+        this.videojuegoService = videojuegoService;
+    }
+    @RequestMapping("/")
+    public String listarVideojuegos(Model model){
+        List<Videojuego> destacados = videojuegoService.buscarDestacados();
+        model.addAttribute("videojuegos", destacados);
+        return "listado";
+    }
+}
